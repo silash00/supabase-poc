ff671ec6c62852b4f085a7696f46958f706f9598
Silas Henrique <silash.silva00@gmail.com>
2025-02-20 23:25:27 -0300

    feat: added tailwind 4

--- src/components/RootLayout.tsx
+++ src/components/RootLayout.tsx
@@ -2,7 +2,7 @@ import { Link, Outlet } from "@tanstack/react-router";
 
 export function RootLayout() {
   return (
-    <div>
+    <div className="">
       <nav>
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -13,27 +13,61 @@ export default function Dashboard() {
 
   return (
     <div>
-      <h1>Usuários</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Nome</th>
-            <th>Email</th>
-            <th>Data de Criação</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((user) => (
-            <tr key={user.id}>
-              <td>{user.id}</td>
-              <td>{user.name}</td>
-              <td>{user.email}</td>
-              <td>{new Date(user.created_at).toLocaleDateString()}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <h1 className="text-base font-semibold text-gray-900">Usuários</h1>
+      <div className="mt-8 flow-root">
+        <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+            <table className="min-w-full divide-y divide-gray-300">
+              <thead>
+                <tr>
+                  <th
+                    scope="col"
+                    className="py-3.5 pr-3 pl-4 text-left text-sm font-semibold text-gray-900 sm:pl-0"
+                  >
+                    ID
+                  </th>
+                  <th
+                    scope="col"
+                    className="py-3.5 pr-3 pl-4 text-left text-sm font-semibold text-gray-900 sm:pl-0"
+                  >
+                    Nome
+                  </th>
+                  <th
+                    scope="col"
+                    className="py-3.5 pr-3 pl-4 text-left text-sm font-semibold text-gray-900 sm:pl-0"
+                  >
+                    Email
+                  </th>
+                  <th
+                    scope="col"
+                    className="py-3.5 pr-3 pl-4 text-left text-sm font-semibold text-gray-900 sm:pl-0"
+                  >
+                    Data de Criação
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="divide-y divide-gray-200">
+                {users.map((user) => (
+                  <tr key={user.id}>
+                    <td className="py-4 pr-3 pl-4 text-sm font-medium whitespace-nowrap text-gray-900 sm:pl-0">
+                      {user.id}
+                    </td>
+                    <td className="py-4 pr-3 pl-4 text-sm font-medium whitespace-nowrap text-gray-900 sm:pl-0">
+                      {user.name}
+                    </td>
+                    <td className="py-4 pr-3 pl-4 text-sm font-medium whitespace-nowrap text-gray-900 sm:pl-0">
+                      {user.email}
+                    </td>
+                    <td className="py-4 pr-3 pl-4 text-sm font-medium whitespace-nowrap text-gray-900 sm:pl-0">
+                      {new Date(user.created_at).toLocaleDateString()}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
